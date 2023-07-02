01bd1ab97a920f233a0a423e51417ed66ea4d82b
Timofey Voronov <timmycarousel@gmail.com>
2023-07-02 21:32:30 +0300

    ПР13

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use((req, res, next) => {
 app.use((error, req, res) => {
   res.status(error.status || 500);
   res.json({
-    error: {
+    message: {
       message: error.message || 'Internal Server Error',
     },
   });
