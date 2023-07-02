175e9f186c0e1c3fb5ec39b2e6b9bd5b817e5b82
Timofey Voronov <timmycarousel@gmail.com>
2023-07-02 15:39:31 +0300

    ПР13

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,6 +1,6 @@
 const Card = require('../models/card');
 
-// GET /cards — возвращает все карточки
+// GET /cards - возвращает все карточки
 const getCards = async (req, res) => {
   try {
     const cards = await Card.find();
@@ -10,31 +10,31 @@ const getCards = async (req, res) => {
   }
 };
 
-// POST /cards — создаёт карточку
+// POST /cards - создаёт карточку
 const createCard = async (req, res) => {
   try {
     const { name, link } = req.body;
     const card = await Card.create({ name, link, owner: req.user._id });
     res.status(201).json(card);
   } catch (error) {
-    res.status(500).json({ error: 'Ошибка сервера' });
+    res.status(400).json({ error: 'Переданы некорректные данные при создании карточки' });
   }
 };
 
-// DELETE /cards/:cardId — удаляет карточку по идентификатору
+// DELETE /cards/:cardId - удаляет карточку по идентификатору
 const deleteCard = async (req, res) => {
   try {
     const card = await Card.findByIdAndRemove(req.params.cardId);
     if (!card) {
-      return res.status(404).json({ error: 'Карточка не найдена' });
+      return res.status(404).json({ error: 'Передан несуществующий _id карточки' });
     }
     return res.status(200).json(card);
   } catch (error) {
-    return res.status(500).json({ error: 'Ошибка сервера' });
+    return res.status(500).json({ error: 'Внутренняя ошибка сервера' });
   }
 };
 
-// PUT /cards/:cardId/likes — поставить лайк карточке
+// PUT /cards/:cardId/likes - добавляет лайк карточке
 const likeCard = async (req, res) => {
   try {
     const card = await Card.findByIdAndUpdate(
@@ -44,10 +44,11 @@ const likeCard = async (req, res) => {
     );
     res.status(200).json(card);
   } catch (error) {
-    res.status(500).json({ error: 'Ошибка сервера' });
+    res.status(400).json({ error: 'Переданы некорректные данные для постановки/снятия лайка' });
   }
 };
-// DELETE /cards/:cardId/likes — убрать лайк с карточки
+
+// DELETE /cards/:cardId/likes - убирает лайк с карточки
 const dislikeCard = async (req, res) => {
   try {
     const card = await Card.findByIdAndUpdate(
@@ -57,9 +58,10 @@ const dislikeCard = async (req, res) => {
     );
     res.status(200).json(card);
   } catch (error) {
-    res.status(500).json({ error: 'Ошибка сервера' });
+    res.status(400).json({ error: 'Переданы некорректные данные для снятия лайка' });
   }
 };
+
 module.exports = {
   getCards,
   createCard,
--- controllers/users.js
+++ controllers/users.js
@@ -1,17 +1,15 @@
 const User = require('../models/user');
 
 const ERROR_CODE = 400;
+const SERVER_ERROR_CODE = 500;
 
-// GET /users — возвращает всех пользователей
-
+// GET /users - возвращает всех пользователей
 const getUsers = async (req, res) => {
   try {
     const users = await User.find({});
     res.status(200).json(users);
   } catch (error) {
-    res
-      .status(ERROR_CODE)
-      .json({ message: 'Ошибка при получении пользователей' });
+    res.status(SERVER_ERROR_CODE).json({ message: 'Внутренняя ошибка сервера' });
   }
 };
 
@@ -20,26 +18,26 @@ const getUserById = async (req, res) => {
   try {
     const user = await User.findById(req.params.userId);
     if (!user) {
-      return res.status(404).json({ message: 'Пользователь не найден' });
+      return res.status(404).json({ message: 'Пользователь по указанному _id не найден' });
     }
     return res.status(200).json(user);
   } catch (error) {
-    return res.status(ERROR_CODE).json({ message: 'Ошибка при получении пользователя' });
+    return res.status(SERVER_ERROR_CODE).json({ message: 'Внутренняя ошибка сервера' });
   }
 };
 
-// POST /users — создаёт пользователя
+// POST /users - создаёт пользователя
 const createUser = async (req, res) => {
   try {
     const { name, about, avatar } = req.body;
     const user = await User.create({ name, about, avatar });
     return res.status(201).json(user);
   } catch (error) {
-    return res.status(ERROR_CODE).json({ message: 'Ошибка при создании пользователя' });
+    return res.status(ERROR_CODE).json({ message: 'Переданы некорректные данные при создании пользователя' });
   }
 };
 
-// PATCH /users/:userId - обновляет профиль пользователя
+// PATCH /users/me - обновляет информацию о пользователе
 const updateUser = async (req, res) => {
   try {
     const { name, about } = req.body;
@@ -49,11 +47,11 @@ const updateUser = async (req, res) => {
       { new: true },
     );
     if (!user) {
-      return res.status(404).json({ message: 'Пользователь не найден' });
+      return res.status(404).json({ message: 'Пользователь с указанным _id не найден' });
     }
     return res.status(200).json(user);
   } catch (error) {
-    return res.status(ERROR_CODE).json({ message: 'Ошибка при обновлении профиля пользователя' });
+    return res.status(ERROR_CODE).json({ message: 'Переданы некорректные данные при обновлении профиля' });
   }
 };
 
