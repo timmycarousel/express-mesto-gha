c2f20a6c79a70198cebffd1b246e71e9e06e4b1f
Timofey Voronov <timmycarousel@gmail.com>
2023-07-02 21:26:30 +0300

    ПР13

--- app.js
+++ app.js
@@ -33,4 +33,19 @@ app.listen(3000, () => {
   // console.log('Сервер запущен на порту 3000');
 });
 
+app.use((req, res, next) => {
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message || 'Internal Server Error',
+    },
+  });
+});
+
 module.exports = app;
